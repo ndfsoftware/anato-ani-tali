e83840636e61e40d3463ccf1e1133a9e8c628228
Ndifabio <nicolasdifabio92@gmial.com>
2026-04-25 21:46:45 -0300

    se acomodan unas rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,11 +11,11 @@ export const routes: Routes = [
       { path: 'about', loadComponent: () => import('@features/about-page/about-page') },
       { path: 'faq', loadComponent: () => import('@features/faq-page/faq-page') },
       { path: 'contact', loadComponent: () => import('@features/contact-page/contact-page') },
-      { path: 'courses', loadComponent: () => import('@features/courses-page/courses-page') },
       {
         path: 'courses/:slug',
         loadComponent: () => import('@features/courses-detail-page/courses-detail-page'),
       },
+      { path: 'courses', loadComponent: () => import('@features/courses-page/courses-page') },
       { path: 'not-found', loadComponent: () => import('@core/components/not-found/not-found') },
       {
         path: 'admin/login',
